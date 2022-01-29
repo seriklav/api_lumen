8ce32b1c490b61ebf83edea9fd34868853c870b1
serik_lav <serik_lav@mail.ru>
2022-01-29 14:04:05 +0200

    Change web => api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -110,7 +110,7 @@ $app->configure('database');
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
-    require __DIR__.'/../routes/web.php';
+    require __DIR__.'/../routes/api.php';
 });
 
 return $app;
